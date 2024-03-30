b0a8585a529b512850928b086379e867e8b7101b
Koji Hasegawa <hasegawa@hubsys.co.jp>
2024-03-30 13:41:34 +0900

    Add same namespace tests

---
+++ Assets/SameNamespace.SubNamespace.Tests/SameNamespace/SubNamespace/NamespaceTest.cs
@@ -0,0 +1,18 @@
+// Copyright (c) 2024 Koji Hasegawa.
+// This software is released under the MIT License.
+
+using NUnit.Framework;
+using UnityEngine;
+
+namespace SameNamespace.SubNamespace
+{
+    [TestFixture]
+    public class NamespaceTest
+    {
+        [Test]
+        public void ExistSameNameSpace()
+        {
+            Debug.Log($"{GetType().FullName}");
+        }
+    }
+}
